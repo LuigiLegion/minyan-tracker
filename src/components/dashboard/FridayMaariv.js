4889260344a57a642efafadd40e4747a257c7aa3
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-17 23:41:13 -0500

    update: created FridayMaariv component

---
+++ src/components/dashboard/FridayMaariv.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FridayMaariv = props => {
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">
+            <span className="bold-text-style">Friday Maariv</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FridayMaariv;
