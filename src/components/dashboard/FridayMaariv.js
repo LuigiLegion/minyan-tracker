602068f75c66643e973b7c7adb8f88cd9eb7bde7
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-18 21:18:54 -0500

    feat: added logic to map going and not going user names in FridayMaariv component

--- src/components/dashboard/FridayMaariv.js
+++ src/components/dashboard/FridayMaariv.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const FridayMaariv = props => {
+const FridayMaariv = ({ attendance }) => {
+  // console.log('FridayMaariv attendance: ', attendance);
+
+  const { going, notGoing } = attendance;
+
   return (
     <div className="section">
       <div className="card z-depth-0">
@@ -8,6 +12,38 @@ const FridayMaariv = props => {
           <span className="card-title">
             <span className="bold-text-style">Friday Maariv</span>
           </span>
+
+          <span className="bold-text-style blue-text-color">Going</span>
+
+          <ul className="going">
+            {going &&
+              going.map(curUser => {
+                const { firstName, lastName } = curUser;
+
+                return (
+                  <li key={curUser.id}>
+                    <span>{`${firstName} ${lastName}`}</span>
+                  </li>
+                );
+              })}
+          </ul>
+
+          <hr />
+
+          <span className="bold-text-style blue-text-color">Not Going</span>
+
+          <ul className="not-going">
+            {notGoing &&
+              notGoing.map(curUser => {
+                const { firstName, lastName } = curUser;
+
+                return (
+                  <li key={curUser.id}>
+                    <span>{`${firstName} ${lastName}`}</span>
+                  </li>
+                );
+              })}
+          </ul>
         </div>
       </div>
     </div>
