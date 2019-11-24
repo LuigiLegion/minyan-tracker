613ab67aa9c50a73522afa49e5acdeb13113c35c
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-24 16:00:55 -0500

    feat: mapped the updates collection to Dashboard component rather than the users collection to retain real time updates while retaining privacy

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -40,7 +40,7 @@ const mapStateToProps = state => {
   return {
     auth: state.firebase.auth,
     profile: state.firebase.profile,
-    users: state.firestore.ordered.users,
+    updates: state.firestore.ordered.updates,
     notifications: state.firestore.ordered.notifications,
   };
 };
@@ -58,7 +58,8 @@ export default compose(
   ),
   firestoreConnect([
     {
-      collection: 'users',
+      collection: 'updates',
+      orderBy: ['timestamp', 'desc'],
     },
     {
       collection: 'notifications',
@@ -72,6 +73,7 @@ export default compose(
 Days.propTypes = {
   auth: PropTypes.object,
   profile: PropTypes.object,
+  updates: PropTypes.object,
   notifications: PropTypes.array,
   getUserDataThunk: PropTypes.func,
 };
