9765c878ef6dc2b8c849c9a4f6cabcd2c9279d8e
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-27 15:55:39 -0500

    feat: added prop types and a mnual test for props in SignedOutLinks component

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,11 +1,14 @@
 // Imports
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import contactUsEmail from '../../config/emailConfig';
 
 // Component
 const SignedOutLinks = props => {
+  // console.log('props in SignedOutLinks: ', props);
+
   return (
     <ul className="right">
       <li>
@@ -34,3 +37,8 @@ const SignedOutLinks = props => {
 };
 
 export default SignedOutLinks;
+
+// Prop Types
+SignedOutLinks.propTypes = {
+  props: PropTypes.object,
+};
