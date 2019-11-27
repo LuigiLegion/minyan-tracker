7e3e1669d8653a846a70378d57275c89d5e617f8
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-27 16:11:43 -0500

    feat: plugged the PageNotFound component into the App component and added a catch-all wildcard route to serve that component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import AdminPanel from './components/admin/AdminPanel';
+import PageNotFound from './components/404/PageNotFound';
+import Dashboard from './components/dashboard/Dashboard';
 
 // Component
 class App extends Component {
@@ -17,9 +18,10 @@ class App extends Component {
           <Navbar>MinyanTracker</Navbar>
 
           <Switch>
-            <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/signin" component={SignIn} />
             <Route path="/admin" component={AdminPanel} />
+            <Route path="/:wildcard" component={PageNotFound} />
             <Route path="/" component={Dashboard} />
           </Switch>
         </div>
