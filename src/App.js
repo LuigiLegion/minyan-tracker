7c7834e8e9272020aeaf67e86a848db037b34606
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-23 14:56:25 +0200

    update: refactored the App component as a functional component and plugged prop types into it

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 // Imports
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import Navbar from './components/layout/Navbar';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import AdminPanel from './components/admin/AdminPanel';
-import PageNotFound from './components/404/PageNotFound';
 import Dashboard from './components/dashboard/Dashboard';
+import PageNotFound from './components/404/PageNotFound';
 
 // Component
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar>Minyan Tracker</Navbar>
+const App = props => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar>Minyan Tracker</Navbar>
 
-          <Switch>
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/signin" component={SignIn} />
-            <Route exact path="/admin" component={AdminPanel} />
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/:wildcard" component={PageNotFound} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+        <Switch>
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/admin" component={AdminPanel} />
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/:wildcard" component={PageNotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
+
+// Prop Types
+App.propTypes = {
+  props: PropTypes.object,
+};
