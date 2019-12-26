c0644d33ae5d79a5057565b026cfc55680b16ec1
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-26 10:14:08 +0200

    chore: removed unnecessary code from the Days component

--- src/components/dashboard/Days.js
+++ src/components/dashboard/Days.js
@@ -3,39 +3,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-// import FridayMaariv from './FridayMaariv';
-// import SaturdayShacharit from './SaturdayShacharit';
-// import SundayMincha from './SundayMincha';
-// import MondayMincha from './MondayMincha';
-// import TuesdayMincha from './TuesdayMincha';
-// import WednesdayMincha from './WednesdayMincha';
-// import ThursdayMincha from './ThursdayMincha';
-// import FridayMincha from './FridayMincha';
 import SingleService from './SingleService';
 
 // Component
 const Days = ({ attendance }) => {
   // console.log('attendance in Days: ', attendance);
-
-  // const {
-  //   friday,
-  //   saturday,
-  //   sunday,
-  //   monday,
-  //   tuesday,
-  //   wednesday,
-  //   thursday,
-  //   fridayMincha,
-  // } = attendance;
-
-  // console.log('friday in Days: ', friday);
-  // console.log('saturday in Days: ', saturday);
-  // console.log('sunday in Days: ', sunday);
-  // console.log('monday in Days: ', monday);
-  // console.log('tuesday in Days: ', tuesday);
-  // console.log('wednesday in Days: ', wednesday);
-  // console.log('thursday in Days: ', thursday);
-  // console.log('fridayMincha in Days: ', fridayMincha);
 
   const attendanceArr = [
     attendance.friday,
@@ -52,22 +24,6 @@ const Days = ({ attendance }) => {
 
   return (
     <div className="col s12 m6">
-      {/* <FridayMaariv friday={friday} />
-
-      <SaturdayShacharit saturday={saturday} />
-
-      <SundayMincha sunday={sunday} />
-
-      <MondayMincha monday={monday} />
-
-      <TuesdayMincha tuesday={tuesday} />
-
-      <WednesdayMincha wednesday={wednesday} />
-
-      <ThursdayMincha thursday={thursday} />
-
-      <FridayMincha fridayMincha={fridayMincha} /> */}
-
       {attendanceArr.map(curService => (
         <SingleService
           key={`${curService.day} ${curService.type}`}
