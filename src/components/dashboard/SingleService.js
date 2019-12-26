d4996a5b996c9df9a935b5745790bb58fcd1049e
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-26 10:10:48 +0200

    feat: created the SingleService component to map through day creaton with rather than map individual day components

---
+++ src/components/dashboard/SingleService.js
@@ -0,0 +1,80 @@
+// Imports
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Component
+const SingleService = ({ service }) => {
+  // console.log('service in SingleService: ', service);
+
+  const { day, type, going, notGoing } = service;
+
+  // console.log('day in SingleService: ', day);
+  // console.log('type in SingleService: ', type);
+  // console.log('going in SingleService: ', going);
+  // console.log('notGoing in SingleService: ', notGoing);
+
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">
+            <span className="bold-text-style">{`${day} ${type}`}</span>
+          </span>
+
+          <span className="bold-text-style blue-text-color">{`Going (${going.length})`}</span>
+
+          <ul className="going">
+            {!going.length ? (
+              <li>
+                <span className="bold-text-style">None</span>
+              </li>
+            ) : (
+              going.map(curUser => {
+                const { fullName } = curUser;
+
+                return (
+                  <li key={curUser.email}>
+                    <span>{fullName}</span>
+
+                    {/* <span className="italic-text-style"> ({gender})</span> */}
+                  </li>
+                );
+              })
+            )}
+          </ul>
+
+          <hr />
+
+          <span className="bold-text-style blue-text-color">{`Not Going (${notGoing.length})`}</span>
+
+          <ul className="not-going">
+            {!notGoing.length ? (
+              <li>
+                <span className="bold-text-style">None</span>
+              </li>
+            ) : (
+              notGoing.map(curUser => {
+                const { fullName } = curUser;
+
+                return (
+                  <li key={curUser.email}>
+                    <span>{fullName}</span>
+
+                    {/* <span className="italic-text-style"> ({gender})</span> */}
+                  </li>
+                );
+              })
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleService;
+
+// Prop Types
+SingleService.propTypes = {
+  service: PropTypes.object,
+};
