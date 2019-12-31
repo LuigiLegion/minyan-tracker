c19e7c4c7eeb77c7182126fdc10345adcf306658
Tal Luigi <luigi.t.agr@gmail.com>
2020-01-01 00:22:05 +0200

    feat: added the congregation name to the notification creation cloud function and plugged it into the Notifications component

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -8,33 +8,39 @@ const Notifications = ({ notifications }) => {
   // console.log('notifications in Notifications: ', notifications);
 
   return (
-    <div className="section">
-      <div className="card z-depth-0">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">
-            <span className="bold-text-style">Notifications</span>
-          </span>
+    <div className="col s12 m5 offset-m1">
+      <div className="section">
+        <div className="card z-depth-0">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">
+              <span className="bold-text-style">Notifications</span>
+            </span>
 
-          <ul className="notifications">
-            {notifications &&
-              notifications.map(curNotification => {
-                return (
-                  <li key={curNotification.id}>
-                    <span className="blue-text-color">
-                      <span className="bold-text-style">
-                        {curNotification.user}
+            <ul className="notifications">
+              {notifications &&
+                notifications.map(curNotification => {
+                  return (
+                    <li key={curNotification.id}>
+                      <span className="blue-text-color">
+                        <span className="bold-text-style">
+                          {curNotification.user}
+                        </span>
                       </span>
-                    </span>
 
-                    <span>{curNotification.content}</span>
+                      <span>{curNotification.content}</span>
 
-                    <div className="grey-text note-date">
-                      {moment(curNotification.timestamp.toDate()).fromNow()}
-                    </div>
-                  </li>
-                );
-              })}
-          </ul>
+                      <span className="bold-text-style">
+                        {curNotification.congregation}
+                      </span>
+
+                      <div className="grey-text note-date">
+                        {moment(curNotification.timestamp.toDate()).fromNow()}
+                      </div>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
