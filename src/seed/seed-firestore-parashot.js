1bb6cb88c0d6fac0374c48513200406fed34aaf3
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-10 02:06:21 -0500

    feat: created seed-firestore-services and a dedicated parashot.json data file

---
+++ src/seed/seed-firestore-parashot.js
@@ -0,0 +1,20 @@
+const firebase = require('../config/fbConfig');
+require('firebase/firestore');
+const firestore = firebase.firestore();
+
+const { parashot } = require('../data/parashot');
+
+const seedCollection = async (collectionName, documentName, dataset) => {
+  try {
+    await firestore
+      .collection(collectionName)
+      .doc(documentName)
+      .set({ counter: 0, [documentName]: dataset });
+
+    console.log('Document added successfully!');
+  } catch (error) {
+    console.error('Error adding document: ', error);
+  }
+};
+
+seedCollection('parashot', 'parashot', parashot);
