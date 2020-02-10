665f33d8f116d89b9c50054bd2238dd32b0dd445
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-10 11:31:44 -0500

    feat: refactored the Welcome component into a stateful functional component using hooks to display the weekly parasha by using the parashot collection from firestore

--- src/components/dashboard/Welcome.js
+++ src/components/dashboard/Welcome.js
@@ -1,11 +1,24 @@
 // Imports
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import PropTypes from 'prop-types';
 
 // Component
-const Welcome = props => {
-  // console.log('props in Welcome: ', props);
+const Welcome = ({ parashotCollection = [] }) => {
+  const [counter, setCounter] = useState(0);
+  const [parashot, setParashot] = useState(['Loading...']);
+
+  useEffect(() => {
+    if (parashotCollection.length) {
+      const parashotDocument = parashotCollection[0];
+      setCounter(parashotDocument.counter);
+      setParashot(parashotDocument.parashot);
+    }
+    // eslint-disable-next-line
+  }, [parashotCollection]);
 
   return (
     <div className="col s12 m6">
@@ -13,16 +26,12 @@ const Welcome = props => {
         <div className="card z-depth-0">
           <div className="card-content grey-text text-darken-3">
             <span className="card-title">
-              <span className="bold-text-style">Placeholder</span>
+              <span className="bold-text-style">Services</span>
             </span>
 
-            <span className="bold-text-style blue-text-color">Placeholder</span>
+            <span className="bold-text-style">This Week's Parasha: </span>
 
-            <ul className="placeholder">
-              <li>
-                <span className="bold-text-style">Placeholder</span>
-              </li>
-            </ul>
+            <span className="italic-text-style">{parashot[counter]}</span>
 
             <ul>
               <li>
@@ -56,9 +65,21 @@ const Welcome = props => {
   );
 };
 
-export default Welcome;
+// Container
+const mapStateToProps = state => ({
+  parashotCollection: state.firestore.ordered.parashot,
+});
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    {
+      collection: 'parashot',
+    },
+  ])
+)(Welcome);
 
 // Prop Types
 Welcome.propTypes = {
-  props: PropTypes.object,
+  parashotCollection: PropTypes.array,
 };
