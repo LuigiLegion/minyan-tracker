e5c4118870878ce8a9b726a165134d12f4f7f6ec
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-16 12:28:19 -0500

    feat: refactored the AdminPanel class component into a functional component

--- src/components/admin/AdminPanel.js
+++ src/components/admin/AdminPanel.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/button-has-type */
 
 // Imports
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Redirect, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
@@ -11,11 +11,15 @@ import PropTypes from 'prop-types';
 import { resetUsersAttendanceThunkCreator } from '../../store/reducers/adminReducer';
 
 // Component
-class AdminPanel extends PureComponent {
-  handleSubmit = event => {
+const AdminPanel = ({
+  auth,
+  profile,
+  users,
+  disabled,
+  resetUsersAttendanceThunk,
+}) => {
+  const handleSubmit = event => {
     event.preventDefault();
-
-    const { users, resetUsersAttendanceThunk } = this.props;
 
     const resetConfirmation = window.confirm(
       'Are you sure you want to reset all users check-in status?'
@@ -26,57 +30,53 @@ class AdminPanel extends PureComponent {
     }
   };
 
-  render() {
-    const { auth, profile, disabled } = this.props;
+  if (!auth.uid) {
+    return <Redirect to="/signin" />;
+  } else if (!profile.isAdmin) {
+    return <Redirect to="/" />;
+  } else {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m12">
+            <div className="section">
+              <div className="card z-depth-0">
+                <div className="card-content grey-text text-darken-3">
+                  <span className="card-title">
+                    <span className="bold-text-style">Admin Panel</span>
+                  </span>
 
-    if (!auth.uid) {
-      return <Redirect to="/signin" />;
-    } else if (!profile.isAdmin) {
-      return <Redirect to="/" />;
-    } else {
-      return (
-        <div className="dashboard container">
-          <div className="row">
-            <div className="col s12 m12">
-              <div className="section">
-                <div className="card z-depth-0">
-                  <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">
-                      <span className="bold-text-style">Admin Panel</span>
-                    </span>
+                  <form className="admin-form" onSubmit={handleSubmit}>
+                    <h5 className="grey-text text-darken-3">Attendance:</h5>
 
-                    <form className="admin-form" onSubmit={this.handleSubmit}>
-                      <h5 className="grey-text text-darken-3">Attendance:</h5>
+                    <button
+                      className="btn waves-effect waves-light blue lighten-1 z-depth-0 admin-reset-button"
+                      disabled={disabled}
+                    >
+                      Reset
+                    </button>
+                  </form>
 
-                      <button
-                        className="btn waves-effect waves-light blue lighten-1 z-depth-0 admin-reset-button"
-                        disabled={disabled}
-                      >
-                        Reset
-                      </button>
-                    </form>
+                  <br />
 
-                    <br />
-
-                    <ul>
-                      <li>
-                        <NavLink to="/">
-                          <span className="bold-text-style">
-                            ← Back To Main Page
-                          </span>
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </div>
+                  <ul>
+                    <li>
+                      <NavLink to="/">
+                        <span className="bold-text-style">
+                          ← Back To Main Page
+                        </span>
+                      </NavLink>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
-}
+};
 
 // Container
 const mapStateToProps = state => ({
