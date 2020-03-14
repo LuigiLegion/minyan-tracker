ecad920e108e7ba2e8b8a4820fc468f72cb5eae7
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-13 23:58:38 -0700

    feat: created pathReducer complete with an action type, action creator, and reducer, and plugged it into rootReducer as path

---
+++ src/store/reducers/pathReducer.js
@@ -0,0 +1,29 @@
+// Initial State
+const initialState = {
+  path: '',
+};
+
+// Action Types
+const GOT_PATH = 'GOT_PATH';
+
+// Action Creators
+export const gotPathActionCreator = path => ({
+  type: GOT_PATH,
+  path,
+});
+
+// Reducer
+const pathReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GOT_PATH:
+      return {
+        ...state,
+        path: action.path,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default pathReducer;
