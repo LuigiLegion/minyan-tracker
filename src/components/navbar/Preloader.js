5cee8699903915bb04bea7f3205fa379e6999f0c
Tal Luigi <luigi.t.agr@gmail.com>
2020-11-06 23:54:40 -0500

    feat: created the Preloader component

---
+++ src/components/navbar/Preloader.js
@@ -0,0 +1,14 @@
+// Imports
+import React from 'react';
+
+// Component
+const Preloader = () => {
+  return (
+    <div className="progress preloader preloader-inner-color">
+      <div className="indeterminate preloader-outer-color" />
+    </div>
+  );
+};
+
+// Exports
+export default Preloader;
