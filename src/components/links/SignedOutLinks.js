72d9b80434ece85e9ef163fcae3d5123e6dd4631
Tal Luigi <luigi.t.agr@gmail.com>
2022-03-25 19:17:10 -0400

    chore: removed nested span elements across NavLink and anchor elements in the SignedOutLinks component

--- src/components/links/SignedOutLinks.js
+++ src/components/links/SignedOutLinks.js
@@ -7,33 +7,41 @@ const SignedOutLinks = () => {
   return (
     <ul className="right">
       <li>
-        <NavLink to="/">
-          <span className="text-style-bold navbar-text-color">
-            Hello, guest.
-          </span>
+        <NavLink
+          className="navbar-text-color"
+          to="/"
+        >
+          Hello, guest.
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink
+          className="text-style-bold navbar-text-color"
+          to="/signin"
+        >
+          Sign In
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink
+          className="text-style-bold navbar-text-color"
+          to="/signup"
+        >
+          Sign Up
         </NavLink>
       </li>
 
       <li>
         <a
+          className="text-style-bold navbar-text-color padding-right"
           href={`mailto:${process.env.REACT_APP_CONTACT_US_EMAIL}`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="text-style-bold navbar-text-color">Contact Us</span>
+          Contact Us
         </a>
-      </li>
-
-      <li>
-        <NavLink to="/signin">
-          <span className="text-style-bold navbar-text-color">Sign In</span>
-        </NavLink>
-      </li>
-
-      <li>
-        <NavLink to="/signup">
-          <span className="text-style-bold navbar-text-color">Sign Up</span>
-        </NavLink>
       </li>
     </ul>
   );
