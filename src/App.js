24ee60224c6603eaafbee0b555ddfdf14333d888
Tal Luigi <luigi.t.agr@gmail.com>
2022-03-30 23:56:33 -0400

    feat: replaced the Switch and Routes in the App component with the Routes component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 // Imports
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import {
-  Navbar,
-  SignIn,
-  SignUp,
-  AdminPanel,
-  Dashboard,
-  Shacharit,
-  Mincha,
-  Maariv,
-  Shabbat,
-  PageNotFound,
-} from './components';
+import { Navbar } from './components';
+import Routes from './Routes';
 
 // Component
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
+    <div className="App">
+      <Navbar />
 
-        <Switch>
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/admin" component={AdminPanel} />
-          <Route exact path="/shacharit" component={Shacharit} />
-          <Route exact path="/mincha" component={Mincha} />
-          <Route exact path="/maariv" component={Maariv} />
-          <Route exact path="/shabbat" component={Shabbat} />
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/:wildcard" component={PageNotFound} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+      <Routes />
+    </div>
   );
 };
 
