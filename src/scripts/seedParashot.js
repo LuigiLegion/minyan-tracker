6e5acea4c1399420fd2fe343bed9519a69623ab7
Tal Luigi <luigi.t.agr@gmail.com>
2022-05-08 13:09:31 -0400

    feat: updated the parashot import statement in the seedParashot script

--- src/scripts/seedParashot.js
+++ src/scripts/seedParashot.js
@@ -2,7 +2,7 @@
 const firebase = require('firebase/app');
 require('firebase/firestore');
 const firebaseConfig = require('../config/firebaseConfig.json');
-const { parashot } = require('../data/parashot');
+const { parashot } = require('../data/parashot.json');
 
 // Initializations
 firebase.initializeApp(firebaseConfig);
